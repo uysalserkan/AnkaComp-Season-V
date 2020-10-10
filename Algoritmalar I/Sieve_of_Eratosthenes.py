4ab08486edcaec3ebc5f2a25fd7cdb97eb904d9f
Serkan UYSAL <uysalserkan08@gmail.com>
2020-10-10 21:04:53 +0300

    Eratosthenes Kalburu eklendi.

---
+++ Algoritmalar I/Sieve_of_Eratosthenes.py
@@ -0,0 +1,44 @@
+# Author: Serkan UYSAL
+# Python Version: 3.7
+
+from math import floor
+from math import sqrt
+
+
+def s_of_eratosthenes(range_) -> list:
+    index = 1
+    __list__ = []
+
+    while index < range_:
+        __list__.append(index)
+        index += 1
+
+    index = 2
+
+    while index < floor(sqrt(range_)):
+        if __list__[index - 1] != 0:
+            j = index * index
+            while j < range_:
+                __list__[j - 1] = 0
+                j = j + index
+
+        index += 1
+
+    i = 0
+    __list__.sort()
+
+    while i < range_:
+        if __list__[i] == 0:
+            __list__.pop(i)
+            continue
+
+        if __list__[i] == max(__list__):
+            break
+
+        i += 1
+
+    return __list__
+
+
+if __name__ == "__main__":
+    print(s_of_eratosthenes(100000))

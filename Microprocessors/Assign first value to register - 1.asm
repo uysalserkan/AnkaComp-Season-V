1ff9d98146b4be2b7d318fe55a9050b74b37e932
Serkan UYSAL <uysalserkan08@gmail.com>
2020-10-27 22:47:58 +0300

    Ilk dosya yazıldı ve deger registere atandi.

---
+++ Microprocessors/Assign first value to register - 1.asm
@@ -0,0 +1,10 @@
+
+; You may customize this and other start-up templates; 
+; The location of this template is c:\emu8086\inc\0_com_template.txt
+
+org 100h
+
+; add your code here     
+mov ax, 0x1234
+
+ret

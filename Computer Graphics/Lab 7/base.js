a5895996631d9312875883be07b593f877c083d7
Serkan UYSAL <serqkanseko@gmail.com>
2020-12-27 15:07:57 +0300

    First base.html and .js file added. That is a 3D but not looks like.

---
+++ Computer Graphics/Lab 7/base.js
@@ -0,0 +1,78 @@
+var gl;
+
+window.onload = function init() {
+    const canvas = document.querySelector("#glCanvas");
+
+    gl = WebGLUtils.setupWebGL(canvas);
+    if (!gl) {
+        alert("Unable to initialize WebGL. your browser or machine may not support it.");
+        return;
+    }
+
+    var program = initShaders(gl, "vertex-shader", "fragment-shader");
+    gl.useProgram(program);
+
+    var vertices = [
+        vec3(-0.5, -0.5, 0.5),
+        vec3(-0.5, 0.5, 0.5),
+        vec3(0.5, 0.5, 0.5),
+        vec3(0.5, -0.5, 0.5),
+        vec3(-0.5, -0.5, -0.5),
+        vec3(-0.5, 0.5, -0.5),
+        vec3(0.5, 0.5, -0.5),
+        vec3(0.5, -0.5, -0.5),
+    ];
+
+    var vertexColors = [
+        [0.0, 0.0, 0.0, 1.0],
+        [1.0, 0.0, 0.0, 1.0],
+        [1.0, 1.0, 0.0, 1.0],
+        [0.0, 1.0, 0.0, 1.0],
+        [0.0, 0.0, 1.0, 1.0],
+        [1.0, 0.0, 1.0, 1.0],
+        [1.0, 1.0, 1.0, 1.0],
+        [0.0, 1.0, 1.0, 1.0],
+    ];
+
+    var indices = [
+        1, 0, 3, 3, 2, 1,
+        2, 3, 7, 7, 6, 2,
+        6, 5, 1, 1, 2, 6,
+        4, 5, 6, 6, 7, 4,
+        5, 4, 0, 0, 1, 5
+    ];
+
+    var vBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, vBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, flatten(vertices), gl.STATIC_DRAW);
+
+    var vPosition = gl.getAttribLocation(program, "vPosition");
+    gl.vertexAttribPointer(vPosition, 3, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(vPosition);
+
+
+    var cBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, cBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, flatten(vertexColors), gl.STATIC_DRAW);
+
+    var cPosition = gl.getAttribLocation(program, "vColor");
+    gl.vertexAttribPointer(cPosition, 4, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(cPosition);
+
+    var iBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, iBuffer);
+    gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint8Array(indices), gl.STATIC_DRAW);
+
+
+    gl.clearColor(0.8, 0.8, 0.8, 1.0);
+
+    render();
+
+};
+
+
+function render() {
+    gl.clear(gl.COLOR_BUFFER_BIT);
+
+    gl.drawElements(gl.TRIANGLES, 24, gl.UNSIGNED_BYTE, 0);
+}
